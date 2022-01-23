f0675fbdec67104ea4a77c0e5c54f9065a6260cd
German Fica <germafica@gmail.com>
2022-01-23 20:29:08 -0300

    Add maven project

---
+++ src/main/java/com/germanfica/transactionsystem/TransactionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.germanfica.transactionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/germanfica/transactionsystem/TransactionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germanfica.transactionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
