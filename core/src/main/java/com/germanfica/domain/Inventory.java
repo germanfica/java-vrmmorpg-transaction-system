a2cc4ed87a8761c08851114a0a71d4f18a27c417
German Fica <germafica@gmail.com>
2022-03-25 13:50:04 -0300

    Update Inventory

--- core/src/main/java/com/germanfica/domain/Inventory.java
+++ core/src/main/java/com/germanfica/domain/Inventory.java
@@ -1,4 +1,14 @@
 package com.germanfica.domain;
 
 public interface Inventory {
+
+    /**
+     * Muestra el inventario
+     */
+    void show();
+
+    /**
+     * Agrega un GameObject al inventario.
+     */
+    void addGameObject(GameObject gameObject);
 }
