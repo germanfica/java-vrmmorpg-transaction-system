62898e34bb6481dfdb6f58105bc6a3918f68125b
German Fica <germafica@gmail.com>
2022-03-29 18:54:05 -0300

    Add services

---
+++ service/src/main/java/com/germafica/service/GameObjectService.java
@@ -0,0 +1,10 @@
+package com.germafica.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameObjectService {
+    // == fields ==
+
+    // == constructors ==
+}
---
+++ service/src/main/java/com/germafica/service/InventoryService.java
@@ -0,0 +1,12 @@
+package com.germafica.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryService {
+    // == fields ==
+
+    // == constructors ==
+
+    // == methods ==
+}
