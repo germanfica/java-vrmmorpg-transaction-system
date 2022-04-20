8414f1baf225740c86e5f0eb7a075d2f0c8a07b4
German Fica <germafica@gmail.com>
2022-04-20 04:14:30 -0300

    Add InventoryDto

---
+++ service/src/main/java/com/germafica/dto/InventoryDto.java
@@ -0,0 +1,28 @@
+package com.germafica.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.germafica.entity.GameObject;
+import lombok.NonNull;
+
+import java.util.List;
+
+public class InventoryDto {
+    @JsonProperty("id")
+    @NonNull
+    private int id;
+
+    @JsonProperty("slot")
+    @NonNull private int slot;
+
+    @JsonProperty("slots_number")
+    @NonNull private int slotsNumber;
+
+    @JsonProperty("tradable")
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    @NonNull private boolean tradable;
+
+    @JsonProperty("game_objects")
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private List<GameObject> gameObjects;
+}
