ce20549d15761c8bfd1f49ed5493149420605b3b
German Fica <germafica@gmail.com>
2022-04-21 20:27:23 -0300

    Add CharacterRepository

---
+++ service/src/main/java/com/germafica/repository/CharacterRepository.java
@@ -0,0 +1,6 @@
+package com.germafica.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CharacterRepository extends CrudRepository<Character, Integer> {
+}
