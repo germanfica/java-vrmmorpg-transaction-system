3be4648d5f6fc2bc72abf17cc067db6cf786e69f
German Fica <germafica@gmail.com>
2022-04-21 20:24:29 -0300

    Add CharacterService

---
+++ service/src/main/java/com/germafica/service/CharacterService.java
@@ -0,0 +1,12 @@
+package com.germafica.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CharacterService {
+    // == fields ==
+
+    // == constructors ==
+
+    // == methods ==
+}
